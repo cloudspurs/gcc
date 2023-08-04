7135d2a91a7dcec8c9c4acc788906581dbd0f316
Jiajie Chen <c@jia.je>
2023-08-05 00:02:18 +0800

    LoongArch: Introduce loongarch32 target
    Introduce loongarch32 target and ilp32 abi variants. The ilp32d abi
    variant is selected as the default abi of loongarch32 target.
    
    Currently, ilp32 abi can only be used for loongarch32, but in the
    future, it might be possible to use ilp32 abi for loongarch64.
    
    contrib/ChangeLog:
    
    	* config-list.mk: Add loongarch32-linux-gnu*.
    
    gcc/ChangeLog:
    
    	* config.gcc: Add target triple loongarch32-*-*-* and
    	corresponding abi ilp32f, ilp32d and ilp32s.
    	* config/loongarch/genopts/loongarch-strings: Add strings for
    	loongarch32 and ilp32 abi variants.
    	* config/loongarch/genopts/loongarch.opt.in: Add
    	-march=loongarch32 and -mabi=ilp32d/ilp32f/ilp32s.
    	* config/loongarch/gnu-user.h: Add ilp32 abi variants to spec.
    	* config/loongarch/linux.h: Add ABI_LIBDIR for ilp32 abi
    	variants.
    	* config/loongarch/loongarch-c.cc (loongarch_cpu_cpp_builtins):
    	Add builtin definitions for loongarch32 target.
    	* config/loongarch/loongarch-def.cc: Add loongarch32 and ilp32
    	definitions.
    	* config/loongarch/loongarch-def.h: Add loongarch32 and ilp32
    	definitions.
    	* config/loongarch/loongarch-driver.h: Add ilp32 abi variants to
    	spec.
    	* config/loongarch/loongarch-opts.cc: Handle ilp32 abi variants.
    	* config/loongarch/loongarch-opts.h: Add loongarch32 case to
    	macros.
    	* config/loongarch/loongarch-str.h: Add loongarch32 and ilp32
    	strings.
    	* config/loongarch/loongarch.opt: Add -march=loongarch32 and
    	-mabi=ilp32d/ilp32f/ilp32s.
    	* config/loongarch/t-linux: Add ilp32 abi variants to multilib.

--- gcc/config/loongarch/loongarch-c.cc
+++ gcc/config/loongarch/loongarch-c.cc
@@ -50,10 +50,22 @@ loongarch_define_unconditional_macros (cpp_reader *pfile)
   builtin_define ("__loongarch__");
 
   /* Base architecture / ABI.  */
+  if (TARGET_32BIT)
+    {
+      builtin_define ("__loongarch_grlen=32");
+      builtin_define ("__loongarch32");
+    }
   if (TARGET_64BIT)
     {
       builtin_define ("__loongarch_grlen=64");
       builtin_define ("__loongarch64");
+    }
+
+  if (TARGET_ABI_ILP32)
+    {
+      builtin_define ("_ABILP32=3");
+      builtin_define ("_LOONGARCH_SIM=_ABILP32");
+      builtin_define ("__loongarch_ilp32");
     }
 
   if (TARGET_ABI_LP64)
